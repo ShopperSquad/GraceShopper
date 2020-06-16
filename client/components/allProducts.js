5af89a693515fe46ed029023a7ec6da91fa3d032
Raghdaa Barmo <raghdaa.barmo@gmail.com>
2020-06-16 18:27:04 -0400

    serve all products from backend; render cards

---
+++ client/components/allProducts.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchProducts} from '../store/products'
+import Product from './productCard'
+
+export class AllProducts extends React.Component {
+  componentDidMount() {
+    const {getProducts} = this.props
+    getProducts()
+  }
+
+  render() {
+    const {products} = this.props
+    return (
+      <div>
+        {products && products.length
+          ? products.map(product => {
+              return <Product product={product} key={product.id} />
+            })
+          : 'No products'}
+      </div>
+    )
+  }
+}
+
+const mapState = state => ({
+  products: state.products
+})
+
+const mapDispatch = dispatch => ({
+  getProducts: () => dispatch(fetchProducts())
+})
+
+export default connect(mapState, mapDispatch)(AllProducts)
--- server/api/products.js
+++ server/api/products.js
@@ -1,30 +1,11 @@
 const router = require('express').Router()
-const {Game, Console} = require('../db/models')
+const {Game} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
     const games = await Game.findAll()
-    const consoles = await Console.findAll()
-    res.json([...games, ...consoles])
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.get('/games', async (req, res, next) => {
-  try {
-    const games = await Game.findAll()
     res.json(games)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.get('/consoles', async (req, res, next) => {
-  try {
-    const consoles = await Console.findAll()
-    res.json(consoles)
   } catch (error) {
     next(error)
   }
