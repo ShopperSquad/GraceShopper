689f3383ba1dd6729c670be9945263052b69563c
Raghdaa Barmo <raghdaa.barmo@gmail.com>
2020-06-19 18:44:04 -0400

    chore: home page admin ternary

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -6,13 +6,21 @@ import {connect} from 'react-redux'
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const {email, isAdmin} = props
 
   return (
-    <div className="row">
-      <div className="col-10 mx-auto my-2 text-center text-title">
-        <h1 className="text-capitalize font-weight-bold">Welcome, {email}</h1>
+    <div>
+      <div className="row">
+        <div className="col-10 mx-auto my-2 text-center text-title">
+          <h1 className="text-capitalize font-weight-bold">Welcome, {email}</h1>
+        </div>
       </div>
+      {isAdmin ? (
+        <div>
+          <h4> You are an Admin. </h4>
+          <p>Add a product to your online shop:</p>
+        </div>
+      ) : null}
     </div>
   )
 }
@@ -22,7 +30,8 @@ export const UserHome = props => {
  */
 const mapState = state => {
   return {
-    email: state.user.email
+    email: state.user.email,
+    isAdmin: state.user.admin
   }
 }
 
