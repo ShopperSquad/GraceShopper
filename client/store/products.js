1199220cb05a1a30a10e475ca13025c76f297118
Raghdaa Barmo <raghdaa.barmo@gmail.com>
2020-06-19 18:22:00 -0400

    chore: AJAX admin routes

--- client/store/products.js
+++ client/store/products.js
@@ -4,11 +4,23 @@ import axios from 'axios'
  * ACTION TYPES
  */
 export const SET_PRODUCTS = 'GET_PRODUCTS'
+export const ADD_PRODUCT = 'ADD_PRODUCT'
+export const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
 
 /**
  * ACTION CREATORS
  */
 export const setProducts = products => ({type: SET_PRODUCTS, products})
+
+export const addProduct = product => ({
+  type: ADD_PRODUCT,
+  product
+})
+
+export const removeProduct = product => ({
+  type: REMOVE_PRODUCT,
+  product
+})
 
 /**
  * THUNK CREATORS
@@ -24,10 +36,36 @@ export const fetchProducts = () => {
   }
 }
 
+export const postProduct = product => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.post('/api/products', product)
+      dispatch(addProduct(data))
+    } catch (error) {
+      console.log('Error posting product!', error)
+    }
+  }
+}
+
+export const deleteProduct = product => {
+  return async dispatch => {
+    try {
+      await axios.delete(`/api/products/${product.id}`)
+      dispatch(removeProduct(product))
+    } catch (error) {
+      console.log('Error deleting product!', error)
+    }
+  }
+}
+
 const productsReducer = (state = [], action) => {
   switch (action.type) {
     case SET_PRODUCTS:
       return action.products
+    case ADD_PRODUCT:
+      return [...state, action.product]
+    case REMOVE_PRODUCT:
+      return state.filter(product => product.id !== action.product.id)
     default:
       return state
   }
--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,25 +1,51 @@
-import Axios from 'axios'
+import axios from 'axios'
 
-//action type
+//action types
 const SELECT_PRODUCT = 'SELECT_PRODUCT'
-//action creator
-const selectedProduct = product => ({
+const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
+
+//action creators
+export const selectProduct = product => ({
   type: SELECT_PRODUCT,
   product
 })
-//thunk
+export const updateProduct = product => ({
+  type: UPDATE_PRODUCT,
+  product
+})
+
+//thunks
 export const fetchSingleProduct = id => async dispatch => {
   try {
-    const {data} = await Axios.get(`/api/products/${id}`)
-    dispatch(selectedProduct(data))
+    const {data} = await axios.get(`/api/products/${id}`)
+    dispatch(selectProduct(data))
   } catch (error) {
     console.log(error)
   }
 }
+
+export const putProduct = (product, id) => {
+  for (let key in product) {
+    if (product[key] === '') {
+      delete product[key]
+    }
+  }
+  return async dispatch => {
+    try {
+      const {data} = await axios.put(`/api/products/${id}`, product)
+      dispatch(updateProduct(data))
+    } catch (error) {
+      console.log('Error PUTting product!', error)
+    }
+  }
+}
+
 //reducer
 export default function singleProductReducer(state = {}, action) {
   switch (action.type) {
     case SELECT_PRODUCT:
+      return action.product
+    case UPDATE_PRODUCT:
       return action.product
     default:
       return state
