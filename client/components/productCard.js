7c6dc251f5c2b3f1666ce1126ad1f59ceae38247
Karolina <kporcioncula.04@gmail.com>
2020-06-20 12:39:31 -0400

    style: product cart on the homepage and singlePageView

--- client/components/productCard.js
+++ client/components/productCard.js
@@ -11,6 +11,9 @@ const Product = props => {
         <Link to={`/products/${product.id}`}>{product.name}</Link>
         {/* card footer */}
         <div className="card-footer d-flex justify-content-between">
+          <button className="cart-btn text-center" type="button">
+            <i className="fas fa-cart-plus" />
+          </button>
           <p className="align-self-center mb-0">{product.title}</p>
           <h5 className="text-blue font-italic mb-0">
             <span className="mr-1">$</span>
@@ -24,4 +27,44 @@ const Product = props => {
 
 export default Product
 
-const ProductWrapper = styled.div``
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+    padding: 5px 5px;
+  }
+  &:hover {
+    .card {
+      border: 0.4rem solid rgba(0, 0, 0, 5);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 20);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+  .cart-btn {
+    postion: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--mainOrange);
+    border: none;
+    color: var(--mainDark);
+    font-size: 1.4rem;
+    border-radius: 0.4rem 0 0 0;
+    transformtranslate(100%, 100%)
+  transition: all 1s ease-in-out;
+  }
+  .img-container: hover .cart-btn {
+    transform: transalte(0, 0);
+  }
+  .cart-btn:hover {
+    color: var(--mainWhite);
+    cursor: pointer;
+  }
+`
--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchSingleProduct} from '../store/singleProduct'
+import {Link} from 'react-router-dom'
 
 class singleProduct extends Component {
   componentDidMount() {
@@ -10,14 +11,41 @@ class singleProduct extends Component {
   render() {
     const arr = this.props.singleProduct
     return (
-      <div key={arr.id}>
-        <h2> {arr.name} </h2>
-        <img src={arr.imageUrl} width="300px" />
-        <h3>Description: {arr.description}</h3>
-        <h3>Year of Release: {arr.yearOfRelease}</h3>
-        <h3>Quantity: {arr.quantity}</h3>
-        <h3>Console: {arr.console}</h3>
-        <h3>Price: {arr.price}</h3>
+      <div key={arr.id} className="container py-2">
+        <div className="col-10 text-black my-1">
+          <h1>{arr.name}</h1>
+        </div>
+
+        <div className="col-10 col-md-6 my-3">
+          <img src={arr.imageUrl} className="img-fluid" alt="" />
+        </div>
+
+        <div className="col-10 col-md-6 my-3 text-capitalize">
+          <h4 className="text-title mt-3 mb-2">
+            Console : <span className="text-muted">{arr.console}</span>
+          </h4>
+          <h4 className="text-black">
+            <strong>
+              price : <span>$</span>
+              {arr.price}
+            </strong>
+          </h4>
+          <p className="text-capitalize font-weight-bold mt-3 mb-0">
+            description :
+          </p>
+          <p className="text-muted lead">{arr.description}</p>
+          <p className="text-capitalize font-weight-bold mt-3 mb-0">
+            Year of Release :{' '}
+            <span className="text-muted lead">{arr.yearOfRelease}</span>
+          </p>
+
+          <div>
+            <Link to="/">
+              <button type="button"> Back to Products</button>
+            </Link>
+            <button type="button"> Add to Cart </button>
+          </div>
+        </div>
       </div>
     )
   }
