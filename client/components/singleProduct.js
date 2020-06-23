c57b3be0111c465d75e7da5abaec01e82002419f
Katie Escoto <katie.escoto@gmail.com>
2020-06-23 01:35:23 -0400

    added css styling, console dropdown

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -43,62 +43,69 @@ class singleProduct extends Component {
     const arr = this.props.singleProduct
     const {isAdmin} = this.props
     return (
-      <div key={arr.id} className="container py-2">
-        <div className="col-10 text-black my-5">
-          <h1>{arr.name}</h1>
-        </div>
+      <div className="single-game-form">
+        <div className="single-game-form-comp">
+          <div key={arr.id} className="container py-2">
+            <div className="col-10 text-black my-5">
+              <h1>{arr.name}</h1>
+            </div>
 
-        <div className="col-10 col-md-6 my-3">
-          <img src={arr.imageUrl} className="img-fluid" alt={arr.name} />
-        </div>
+            <div className="col-10 col-md-6 my-3">
+              <img src={arr.imageUrl} className="img-fluid" alt={arr.name} />
+            </div>
 
-        <div className="col-10 col-md-6 my-3 text-capitalize">
-          <h4 className="text-black">
-            <strong>
-              {' '}
-              Price : <span>$</span> {arr.price / 100}{' '}
-            </strong>
-          </h4>
-          <p className="text-capitalize font-weight-bold mt-3 mb-0">
-            <span className="text-black lead text-muted">
-              {arr.description}
-            </span>
-          </p>
-          <h5 className="text-title  mt-3 mb-2">
-            Console : <span className="text-uppercase">{arr.console}</span>
-          </h5>
-          <h6 className="text-capitalize font-weight-bold mt-3 mb-0">
-            Quantity :{' '}
-            <span className="text-black lead text-muted">{arr.quantity}</span>
-          </h6>
-          <h6 className="text-capitalize font-weight-bold mt-3 mb-0">
-            Year of Release :{' '}
-            <span className="text-black lead text-mu">{arr.yearOfRelease}</span>
-          </h6>
-        </div>
+            <div className="col-10 col-md-6 my-3 text-capitalize">
+              <h4 className="text-black">
+                <strong>
+                  {' '}
+                  Price : <span>$</span> {arr.price / 100}{' '}
+                </strong>
+              </h4>
+              <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                <span className="text-black lead text-muted">
+                  {arr.description}
+                </span>
+              </p>
+              <h5 className="text-title  mt-3 mb-2">
+                Console : <span className="text-uppercase">{arr.console}</span>
+              </h5>
+              <h6 className="text-capitalize font-weight-bold mt-3 mb-0">
+                Quantity :{' '}
+                <span className="text-black lead text-muted">
+                  {arr.quantity}
+                </span>
+              </h6>
+              <h6 className="text-capitalize font-weight-bold mt-3 mb-0">
+                Year of Release :{' '}
+                <span className="text-black lead text-mu">
+                  {arr.yearOfRelease}
+                </span>
+              </h6>
+            </div>
 
-        <div>
-          <AddToCart
-            singleGame={arr}
-            isLoggedIn={this.props.isLoggedIn}
-            addToStorage={this.addToStorage}
-            addNewGame={this.addNewGame}
-          />
+            <div>
+              <AddToCart
+                singleGame={arr}
+                isLoggedIn={this.props.isLoggedIn}
+                addToStorage={this.addToStorage}
+                addNewGame={this.addNewGame}
+              />
+            </div>
+          </div>
         </div>
 
         {isAdmin ? (
-          <div>
-            <div>
-              <h2>You are an Admin.</h2>
-              <p>
-                To remove this from store: &nbsp;{' '}
-                <button type="button" onClick={this.handleOnClick}>
-                  Remove
-                </button>{' '}
-              </p>
-              <p>You can update this product here:</p>
-            </div>
+          <div className="single-game-form-comp">
             <div className="game-form-box">
+              <div>
+                <h2>You are an Admin.</h2>
+                <p>
+                  To remove this from store: &nbsp;{' '}
+                  <button type="button" onClick={this.handleOnClick}>
+                    Remove
+                  </button>{' '}
+                </p>
+              </div>
               <UpdateGame
                 id={this.props.match.params.id}
                 products={arr.products}
--- client/components/user-home.js
+++ client/components/user-home.js
@@ -14,17 +14,18 @@ export const UserHome = props => {
     <div className="row">
       <div className="col-10 mx-auto my-2 text-center text-title">
         <h1 className="text-capitalize font-weight-bold">Welcome, {email}</h1>
-        <div className="game-form-box" />
       </div>
       {isAdmin ? (
-        <div>
-          <div>
+        <div className="single-game-form">
+          <div className="single-game-form-comp">
             <h3> You are an Admin. </h3>
             <Link to="/user-information">See All Customers</Link>
           </div>
-          <div>
-            <h3>Add a game to your shop?</h3>
-            <AddGame />
+          <div className="single-game-form-comp">
+            <div className="game-form-box">
+              <h3>Add a game to your shop?</h3>
+              <AddGame />
+            </div>
           </div>
         </div>
       ) : null}
