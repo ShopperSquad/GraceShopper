09cc6de5b1fae15314e25f944f2bf0294fec8f47
Raghdaa Barmo <raghdaa.barmo@gmail.com>
2020-06-23 11:41:39 -0400

    fix: admin-only API routes are now restricted

--- server/api/users.js
+++ server/api/users.js
@@ -6,14 +6,17 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email', 'admin'],
-      order: ['id']
-    })
-    res.json(users)
+    if (!req.user) res.sendStatus(401)
+    if (req.user.admin) {
+      const users = await User.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ['id', 'email', 'admin'],
+        order: ['id']
+      })
+      res.json(users)
+    } else res.sendStatus(401)
   } catch (err) {
     next(err)
   }
