a493a1dcebfc9aeb3def4432c39f5f6b2b5201a0
Raghdaa Barmo <raghdaa.barmo@gmail.com>
2020-06-23 14:27:43 -0400

    fix: deleted checkout form & refactored home

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -14,17 +14,23 @@ export const UserHome = props => {
     <div className="row">
       <div className="col-10 mx-auto my-2 text-center text-title">
         <h1 className="text-capitalize font-weight-bold">Welcome, {email}</h1>
-        <div className="game-form-box" />
       </div>
       {isAdmin ? (
         <div>
           <div>
             <h3> You are an Admin. </h3>
+            <Link to="/my-cart">Test Cart Functionality</Link>
+            <br />
             <Link to="/user-information">See All Customers</Link>
           </div>
           <div>
+            <br />
+          </div>
+          <div>
             <h3>Add a game to your shop?</h3>
-            <AddGame />
+            <div className="game-form-box">
+              <AddGame />
+            </div>
           </div>
         </div>
       ) : null}
